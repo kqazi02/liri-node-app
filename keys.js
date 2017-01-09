9f7b9ec618e6605edcc995f308d9b10da17362d6
Kashan Ali Qazi <kashan.qazi02@utexas.edu>
2017-01-08 20:38:53 -0600

    initialized the folder and files. Added twitter Authentication

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log("this is loaded");
+
+exports.twitterKeys = {
+	consumer_key: "CQHkuMxeLNdMggyPaiBFlEiNY",
+	consumer_secret:"qwREY4fwzMmdf271zwdDp6Qs9ezzktu0xIpxtqywqZa6vchPvu",
+	access_token_key:"495241991-aNT6b5lfx9BC4Jf0qSPS2slHnANWtQA6f08NUfTS",
+	access_token_secret:"8H01yO4zQYRZf9agRUTq3fdx5FM8kDAi5D8luU7wwY2ep",
+}
