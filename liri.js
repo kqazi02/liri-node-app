01bf5b6c59cd55900b5bd770e26ea348b5def190
Kashan Ali Qazi <kashan.qazi02@utexas.edu>
2017-01-16 21:42:36 -0600

    finished the spotify function

--- liri.js
+++ liri.js
@@ -5,8 +5,17 @@ var spotify = require("spotify");
 var twitter = require('twitter');
 
 // registering the user input for what the should do
-var action = process.argv[2]
+var action = process.argv[2];
 
+// checks if the user has input additional arguments
+var additionalArgs;
+if (process.argv.length > 2) {
+
+	additionalArgs = "";
+	for(var i = 3; i < process.argv.length; i++){
+		additionalArgs = additionalArgs + process.argv[i] + " ";
+	}
+}
 
 switch (action){
 
@@ -15,7 +24,8 @@ switch (action){
 		break;
 
 	case "spotify-this-song":
-		spotifySong()
+		songName = process.argv[3];
+		spotifySong(songName)
 		break;
 
 	case "movie-this":
@@ -26,6 +36,8 @@ switch (action){
 		justDoIt()
 		break;
 }
+
+// ======================================================================
 
 //This function shows last 20 tweets and their timestamp.
 function tweet(){
@@ -43,6 +55,7 @@ function tweet(){
 		//shows an error, if an error occurs
 		if (error){
 			console.log(error);
+			return;
 		}
 		//cycles through the results, and prints the timestamp and the tweet
 		for(var i = 0; i < tweetCount; i++){
@@ -51,6 +64,78 @@ function tweet(){
 			console.log("");
 		}
 
+		return;
+
 	}) //client.get callback function ends here
 
-} // tweet() function ends here
+} // tweet function ends here
+
+// =======================================================================
+
+//this function shows information about a song that the user requests
+//if a song is not provided, information about "the sign" by Ace of Base is shown
+function spotifySong(){
+
+	// if user specifies a song
+	if (additionalArgs) {
+		spotify.search({ type: 'track', query: additionalArgs, limit: 1 }, function(err, data) {
+			//console log an error, if there is one.
+    		if ( err ) {
+        		console.log('Error occurred: ' + err);
+        		return;
+    		}
+			
+			console.log("Artist(s):")
+			// Loops through the array of artists, and console logs the names
+			for (var i = 0; i < data.tracks.items[1].artists.length; i++){
+				console.log(data.tracks.items[1].artists[i].name);
+			} 
+			console.log("");
+			console.log("Song:");
+			// console logs name of the song
+			console.log(data.tracks.items[1].name);
+			console.log("");
+			console.log("Link:");
+			// console logs preview URL for the song
+			console.log(data.tracks.items[1].preview_url);
+			console.log("");
+			console.log("Album:")
+			// console logs name of the album that the song is from
+			console.log(data.tracks.items[1].album.name);
+
+
+		}); // Callback function ends here
+	}
+	// if user does not specify a song
+	else{
+		spotify.search({ type: 'track', query: 'The Sign', limit: 1 }, function(err, data) {
+			//console log an error, if there is one.
+    		if ( err ) {
+        		console.log('Error occurred: ' + err);
+        		return;
+    		}
+
+			console.log("Artist(s):")
+			// Loops through the array of artists, and console logs the names
+			for (var i = 0; i < data.tracks.items[1].artists.length; i++){
+				console.log(data.tracks.items[1].artists[i].name);
+			} 
+			console.log("");
+			console.log("Song:");
+			// console logs name of the song
+			console.log(data.tracks.items[1].name);
+			console.log("");
+			console.log("Link:");
+			// console logs preview URL for the song
+			console.log(data.tracks.items[1].preview_url);
+			console.log("");
+			console.log("Album:")
+			// console logs name of the album that the song is from
+			console.log(data.tracks.items[1].album.name);
+		}); // callback function ends here
+	}
+
+
+} //spotifySong function ends here
+
+// =======================================================================
